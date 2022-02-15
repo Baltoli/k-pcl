dda35bda28d52f12a77507897b65a048e4ff9dcd
Bruce Collie <brucecollie82@gmail.com>
2022-02-15 16:41:39 +0000

    Redundant rule

---
+++ pcl-kompiled/configVars.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+declaredConfigVar_STDIN='String'
+declaredConfigVar_IO='String'
+declaredConfigVar_PGM='KItem'
+declaredConfigVar_IO='String'
+declaredConfigVars=(
+    'STDIN'
+    'IO'
+    'PGM'
+    'IO'
+)
